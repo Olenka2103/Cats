895eea10811fba01971c85d0b57449f850ec3b4d
Olenka2103 <yakkolga@gmail.com>
2025-07-29 20:37:29 +0300

    создана функция для получения изображения котиков

--- Cats.py
+++ Cats.py
@@ -1,9 +1,20 @@
+from http.client import responses
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from pygame.examples.aliens import load_image
+def load_image():
+    try:
+        response = requests.get(url) #делаем запрос по этой ссылке и то, что получим положим в переменную
+        response.raise_for_status() #обработка исключений - если будет какая то ошибка, тут эту ошибку получим
+        image_data = BytesIO(response.content) #сама картинка будет преобразована с помощью байтс ио
+        img = Image.open(image_data) #имэдж дата открываем с пом библ пиллоу и кладём в переменную локальную
+        return ImageTk.PhotoImage(img) #функция вернёт картинку и мы её положим в пер где эта функция вызвана
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+        return None #обязательно вернём результат, если всё хорошо - картинка, если ошибка - текст ошибки в консоль
+
 
 window = Tk()
 window.title('Cats')
